930d63a81798aef952886375540baff655f7af77
ectionboy <125730248+ectionboy@users.noreply.github.com>
2024-02-10 16:35:41 +0200

    remove comments in guards

--- src/guards/PrivateRoute/PrivateRoute.jsx
+++ src/guards/PrivateRoute/PrivateRoute.jsx
@@ -1,15 +1,3 @@
-// import React from 'react';
-// import { useSelector } from 'react-redux';
-// import { Navigate, Outlet } from 'react-router-dom';
-// import { isLoggedIn } from '../../redux/selectors';
-
-// const PrivateRoute = ({ children }) => {
-//   const isAuth = useSelector(isLoggedIn);
-//   return isAuth ?  children : <Navigate to="/login" />;
-// };
-
-// export default PrivateRoute;
-
 import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
 import { isLoggedIn } from '../../redux/selectors';
--- src/guards/PublicRoute/PublicRoute.jsx
+++ src/guards/PublicRoute/PublicRoute.jsx
@@ -1,16 +1,3 @@
-// import React from 'react';
-// import { useSelector } from 'react-redux';
-// import { Navigate } from 'react-router-dom';
-// import { isLoggedIn } from '../../redux/selectors';
-
-// const PublicRoute = ({ children }) => {
-//   const isAuth = useSelector(isLoggedIn);
-
-//   return !isAuth ? children : <Navigate to="/" />;
-// };
-
-// export default PublicRoute;
-
 import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
 import { isLoggedIn } from '../../redux/selectors';
