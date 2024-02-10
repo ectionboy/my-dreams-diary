0f087bb80eb38869a66148aadbe49ad6143da62b
ectionboy <125730248+ectionboy@users.noreply.github.com>
2024-02-10 18:26:33 +0200

    add avatar

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Avatar, Box, Button, Typography } from '@mui/material';
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { logOutThunk } from '../../redux/auth/authThunk';
@@ -16,12 +16,21 @@ const UserMenu = () => {
     const handleButton = () => {
         exit();
       };
+
   return (
 <Box
           component="div"
           sx={{ display: 'flex', alignItems: 'baseline', gap: '8px' }}
         >
           <Typography variant="body2">Hi, {name ?? ''}</Typography>
+          <Avatar
+        sx={{ bgcolor: "#092ff3" }}
+        alt="avatar"
+        src="/broken-image.jpg"
+      >
+{typeof name === "string" ? name.trimStart().slice(0, 1).toUpperCase() : "A"} 
+
+  </Avatar>
           <Button
             sx={{ padding: '0px', textTransform: 'none' }}
             variant="outlined"
