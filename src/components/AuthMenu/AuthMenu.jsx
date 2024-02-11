6699d8fda7eb73c20f851382f5bf9c4694a1745c
ectionboy <125730248+ectionboy@users.noreply.github.com>
2024-02-11 02:22:57 +0200

    mod AuthMenu

--- src/components/AuthMenu/AuthMenu.jsx
+++ src/components/AuthMenu/AuthMenu.jsx
@@ -1,36 +1,38 @@
-import { Box, Button } from '@mui/material'
-import React from 'react'
+import { Box, Button, ButtonGroup } from '@mui/material';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const AuthMenu = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const navToLoginPage = () => {
-        navigate('/login');
-      };
-      const navToRegPage = () => {
-        navigate('/register');
-      };
+  const navToLoginPage = () => {
+    navigate('/login');
+  };
+  const navToRegPage = () => {
+    navigate('/register');
+  };
   return (
     <Box>
-                  <Button
-            sx={{}}
-            onClick={navToLoginPage}
-            type="button"
-            size="small"
-          >
-            Sign in
-          </Button>
-          <Button
-            sx={{}}
-            onClick={navToRegPage}
-            type="button"
-            size="small"
-          >
-            Sign up
-          </Button>
+      <ButtonGroup variant="text" aria-label="Authtentificate">
+        <Button
+          sx={{ color: 'inherit' }}
+          onClick={navToLoginPage}
+          type="button"
+          size="small"
+        >
+          Sign in
+        </Button>
+        <Button
+          sx={{ color: 'inherit' }}
+          onClick={navToRegPage}
+          type="button"
+          size="small"
+        >
+          Sign up
+        </Button>
+      </ButtonGroup>
     </Box>
-  )
-}
+  );
+};
 
-export default AuthMenu
+export default AuthMenu;
