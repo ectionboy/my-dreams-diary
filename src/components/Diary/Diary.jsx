5420c6d1f545ef7f4ec25d3129695a153da5aa7a
ectionboy <125730248+ectionboy@users.noreply.github.com>
2024-03-04 00:32:16 +0200

    delete notes object

--- src/components/Diary/Diary.jsx
+++ src/components/Diary/Diary.jsx
@@ -19,73 +19,7 @@ const Diary = () => {
   const notes = useSelector(getNotesItems);
 
 console.log(notes);
-  // const notes2 = [
-  //   {
-  //     _id: '123456gej',
-  //     title: 'Qwerty',
-  //     description: 'Description.......',
-  //     imgUrl:
-  //       'http://www.gravatar.com/avatar/e4102cc7a5201c8dd9f198cdcb3cfc6c?s=200',
-  //     created: '2024-02-19',
-  //     updated: '2024-02-20',
-  //   },
-  //   {
-  //     _id: '123456gedfbdj1',
-  //     title: 'Qwerty',
-  //     description: 'Description.......',
-  //     imgUrl:
-  //       'http://www.gravatar.com/avatar/e4102cc7a5201c8dd9f198cdcb3cfc6c?s=200',
-  //     created: '2024-02-19',
-  //     updated: '2024-02-20',
-  //   },
-  //   {
-  //     _id: '123456gdfbej9',
-  //     title: 'Qwerty',
-  //     description: 'Description.......',
-  //     imgUrl:
-  //       'http://www.gravatar.com/avatar/e4102cc7a5201c8dd9f198cdcb3cfc6c?s=200',
-  //     created: '2024-02-19',
-  //     updated: '2024-02-20',
-  //   },
-  //   {
-  //     _id: '123456fhfhgejf',
-  //     title: 'Qwerty',
-  //     description: 'Description.......',
-  //     imgUrl:
-  //       'http://www.gravatar.com/avatar/e4102cc7a5201c8dd9f198cdcb3cfc6c?s=200',
-  //     created: '2024-02-19',
-  //     updated: '2024-02-20',
-  //   },
-  //   {
-  //     _id: '123456gbfhdsbejs',
-  //     title: 'Qwerty',
-  //     description: 'Description.......',
-  //     imgUrl:
-  //       'http://www.gravatar.com/avatar/e4102cc7a5201c8dd9f198cdcb3cfc6c?s=200',
-  //     created: '2024-02-19',
-  //     updated: '2024-02-20',
-  //   },
-  //   {
-  //     _id: '123456gesdgfjn',
-  //     title: 'Qwerty',
-  //     description: 'Description.......',
-  //     imgUrl:
-  //       'http://www.gravatar.com/avatar/e4102cc7a5201c8dd9f198cdcb3cfc6c?s=200',
-  //     created: '2024-02-19',
-  //     updated: '2024-02-20',
-  //   },
-  //   {
-  //     _id: '123456gemsdj',
-  //     title: 'Qwerty',
-  //     description: 'Description.......',
-  //     imgUrl:
-  //       'http://www.gravatar.com/avatar/e4102cc7a5201c8dd9f198cdcb3cfc6c?s=200',
-  //     created: '2024-02-19',
-  //     updated: '2024-02-20',
-  //   },
-  // ];
-  //   const notes = [];
-  
+
   return (
     <Container
       component="section"
